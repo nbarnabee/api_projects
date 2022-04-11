48f54583582963060351d95431820e1d105dc358
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-11 15:45:40 +0200

    Add basic fetch code

---
+++ nasa/main.js
@@ -0,0 +1,12 @@
+fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+.then(res => res.json()) // parse response as JSON
+.then(data => {
+  console.log(data)
+  //and do whatever else you want to do with it here
+  document.querySelector("img").src=data.url;
+  document.querySelector("figcaption").innerText=data.title;
+  document.querySelector(".explanation").innerText=data.explanation;
+})
+.catch(err => {
+    console.log(`error ${err}`)
+});
