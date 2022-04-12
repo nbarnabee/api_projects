4bb42eff37827bf1d22fb7fac625cf93bffc28da
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-12 09:20:28 +0200

    Add instruction list builder

--- cocktails/main.js
+++ cocktails/main.js
@@ -9,10 +9,22 @@ fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
   .then(res => res.json())
   .then(data => {
     console.log(data);
+    console.log(data.drinks[0]);
+
+    /* --- Building the list of instructions --- */
+    let instructionList = data.drinks[0].strInstructions.split(". ");
+    instructionList.forEach(element => {
+      let instructionPoint = document.createElement("li");
+      instructionPoint.innerText = element;
+      document.querySelector("#instructions").appendChild(instructionPoint);
+    });
+
+    /* --- Building the list of ingredients --- */
+    //let ingredientsList = 
+
     document.querySelector("h2").innerText = data.drinks[0].strDrink;
     document.querySelector("img").src = data.drinks[0].strDrinkThumb;
     document.querySelector("img").alt = `Photograph of a ${searchTerm}`;
-    document.querySelector("#instructions").innerText = data.drinks[0].strInstructions;
   })
   .catch(err => {
     console.log(`error ${err}`)
