d38ff47be0b33abfd5f09a751a51771853906c96
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-12 12:46:39 +0200

    Add code to control card visibility

--- cocktails/main.js
+++ cocktails/main.js
@@ -3,60 +3,61 @@ document.querySelector("input").value = "";
 document.querySelector("button").addEventListener("click", getDrink);
 
 function getDrink() {
-const searchTerm = document.querySelector("input").value
+  const searchTerm = document.querySelector("input").value
 
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
-  .then(res => res.json())
-  .then(data => {
-    console.log(data);
-    console.log(data.drinks[0]);
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data);
+      console.log(data.drinks[0]);
+      document.querySelector(".card").classList.remove("hidden");
 
-    /* --- Building the list of instructions --- */
-    function getInstructions() {
-    let instructionList = [`Select a ${data.drinks[0].strGlass}`];
-    instructionList = instructionList.concat(data.drinks[0].strInstructions.split(". "));
-    makeList(instructionList, "#instructions");
-   };
-
-    /* --- Building the list of ingredients --- */
-    function getIngredients() {
-    let ingredientsList = [];
-    let ingredientVar;
-    let ingredientItem;
-    let ingredientMeasureVar;
-    let ingredientMeasure;
-    for (let i = 1; i <= 15; i++) {
-      ingredientVar = `strIngredient${i}`;
-      ingredientMeasureVar = `strMeasure${i}`;
-      ingredientItem = data.drinks[0][ingredientVar];
-      ingredientMeasure = data.drinks[0][ingredientMeasureVar];
-      if (!ingredientItem)
-        break;
-      else if (ingredientMeasure)
-        ingredientsList.push(`${ingredientMeasure} ${ingredientItem}`);
-      else ingredientsList.push(ingredientItem);
-    }
-    makeList(ingredientsList, "#ingredients");
+      /* --- Building the list of instructions --- */
+      function getInstructions() {
+      let instructionList = [`Select a ${data.drinks[0].strGlass}`];
+      instructionList = instructionList.concat(data.drinks[0].strInstructions.split(". "));
+      makeList(instructionList, "#instructions");
     };
 
-    function makeList(arr, target) {
-      document.querySelector([target]).innerHTML = "";
-      arr.forEach(element => {
-      let listItem = document.createElement("li");
-      listItem.innerText = element;
-      document.querySelector([target]).appendChild(listItem);
-    })};
+      /* --- Building the list of ingredients --- */
+      function getIngredients() {
+      let ingredientsList = [];
+      let ingredientVar;
+      let ingredientItem;
+      let ingredientMeasureVar;
+      let ingredientMeasure;
+      for (let i = 1; i <= 15; i++) {
+        ingredientVar = `strIngredient${i}`;
+        ingredientMeasureVar = `strMeasure${i}`;
+        ingredientItem = data.drinks[0][ingredientVar];
+        ingredientMeasure = data.drinks[0][ingredientMeasureVar];
+        if (!ingredientItem)
+          break;
+        else if (ingredientMeasure)
+          ingredientsList.push(`${ingredientMeasure} ${ingredientItem}`);
+        else ingredientsList.push(ingredientItem);
+      }
+      makeList(ingredientsList, "#ingredients");
+      };
 
-    document.querySelector("h2").innerText = data.drinks[0].strDrink;
-    document.querySelector("img").src = data.drinks[0].strDrinkThumb;
-    getIngredients();
-    getInstructions();
-    document.querySelector("input").value = "";
-  })
+      function makeList(arr, target) {
+        document.querySelector([target]).innerHTML = "";
+        arr.forEach(element => {
+        let listItem = document.createElement("li");
+        listItem.innerText = element;
+        document.querySelector([target]).appendChild(listItem);
+      })};
 
-  .catch(err => {
-    console.log(`error ${err}`)
-    alert("Sorry, we didn't find anything");
-    document.querySelector("input").value = "";
-  })
+      document.querySelector("h2").innerText = data.drinks[0].strDrink;
+      document.querySelector("img").src = data.drinks[0].strDrinkThumb;
+      getIngredients();
+      getInstructions();
+      document.querySelector("input").value = "";
+    })
+
+    .catch(err => {
+      console.log(`error ${err}`)
+      alert("Sorry, we didn't find anything");
+      document.querySelector("input").value = "";
+    })
 }
