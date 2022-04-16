47eacab1ca8c32d996be2128abd6135e6a8defa5
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-16 02:20:13 +0200

    Lay groundwork for video display

--- nasa/main.js
+++ nasa/main.js
@@ -3,7 +3,11 @@ fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
 .then(data => {
   console.log(data)
   //and do whatever else you want to do with it here
-  document.querySelector("img").src=data.url;
+  if (data.media_type==="video") {
+    document.querySelector("source").src=data.url;
+    document.querySelector("video").classList.remove("hidden");
+  }
+    else document.querySelector("img").src=data.url;
   document.querySelector("h1").innerText=`${data.date}:  ${data.title}`;
   document.querySelector(".explanation").innerText=data.explanation;
 })
