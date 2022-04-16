7f3dcfaac84b8fafcc0849800fc218ada0569c93
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-16 22:08:58 +0200

    Add date picker functionality

--- nasa/main.js
+++ nasa/main.js
@@ -1,3 +1,6 @@
+document.querySelector("button").onclick = getByDate;
+let value = "";
+
 fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
 .then(res => res.json()) // parse response as JSON
 .then(data => {
@@ -6,6 +9,7 @@ fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
   if (data.media_type==="video") {
     document.querySelector("iframe").src=data.url;
     document.querySelector("iframe").classList.remove("hidden");
+    document.querySelector("img").src="";
   }
     else document.querySelector("img").src=data.url;
   if (data.copyright)
@@ -17,3 +21,33 @@ fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
 .catch(err => {
     console.log(`error ${err}`)
 });
+
+function getByDate() {
+  value = document.querySelector("input").value;
+  fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${value}`)
+  .then(res => res.json()) // parse response as JSON
+  .then(data => {
+    console.log(data)
+    //and do whatever else you want to do with it here
+    if (data.media_type==="video") {
+      document.querySelector("iframe").src=data.url;
+      document.querySelector("iframe").classList.remove("hidden");
+      document.querySelector("img").src="";
+    }
+      else {
+        if (!document.querySelector("iframe").classList.contains("hidden"))
+          document.querySelector("iframe").classList.add("hidden");
+        document.querySelector("img").src=data.url;
+      }
+
+    if (data.copyright) {
+      document.querySelector(".copyright").innerHTML = `&copy; ${data.copyright}`}
+      else document.querySelector(".copyright").innerHTML = "";
+
+    document.querySelector("h1").innerText=`${data.date}:  ${data.title}`;
+    document.querySelector(".explanation").innerText=data.explanation;
+  })
+  .catch(err => {
+      console.log(`error ${err}`)
+  });
+}
