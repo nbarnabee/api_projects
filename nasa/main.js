67760d8a6cfb2818b90f586c189bdd588d28ac92
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-16 22:28:09 +0200

    Refactoring code

--- nasa/main.js
+++ nasa/main.js
@@ -1,10 +1,10 @@
 document.querySelector("button").onclick = getByDate;
-let baseURL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+let baseURL = "https://api.nasa.gov/planetary/apod?api_key=NGbXFaC948GisO5Nx2TmrXLKYXBa5dVQ2c5OjFKw";
 getPOTD(baseURL);
 
 function getByDate() {
   let dateValue = document.querySelector("input").value;
-  let date = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${dateValue}`;
+  let date = `https://api.nasa.gov/planetary/apod?api_key=NGbXFaC948GisO5Nx2TmrXLKYXBa5dVQ2c5OjFKw&date=${dateValue}`;
   getPOTD(date);
 }
 
@@ -23,38 +23,6 @@ function getPOTD(url) {
     if (data.copyright)
     {console.log(data.copyright);
       document.querySelector(".copyright").innerHTML = `&copy; ${data.copyright}`};
-    document.querySelector("h1").innerText=`${data.date}:  ${data.title}`;
-    document.querySelector(".explanation").innerText=data.explanation;
-  })
-  .catch(err => {
-      console.log(`error ${err}`)
-  });
-}
-
-
-
-function getByDate() {
-  value = document.querySelector("input").value;
-  fetch(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${value}`)
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-    console.log(data)
-    //and do whatever else you want to do with it here
-    if (data.media_type==="video") {
-      document.querySelector("iframe").src=data.url;
-      document.querySelector("iframe").classList.remove("hidden");
-      document.querySelector("img").src="";
-    }
-      else {
-        if (!document.querySelector("iframe").classList.contains("hidden"))
-          document.querySelector("iframe").classList.add("hidden");
-        document.querySelector("img").src=data.url;
-      }
-
-    if (data.copyright) {
-      document.querySelector(".copyright").innerHTML = `&copy; ${data.copyright}`}
-      else document.querySelector(".copyright").innerHTML = "";
-
     document.querySelector("h1").innerText=`${data.date}:  ${data.title}`;
     document.querySelector(".explanation").innerText=data.explanation;
   })
