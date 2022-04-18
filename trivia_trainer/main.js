fde6da379f5feb5c607cddb048bd272a3201cfea
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-18 15:02:55 +0200

    Add answer display

--- trivia_trainer/main.js
+++ trivia_trainer/main.js
@@ -16,7 +16,6 @@ function getQuestions() {
   })
   .catch(error => {
     console.log(`Error: ${error}`);
-    // any additional error messages here
   })
 }
 
@@ -24,22 +23,72 @@ function makeQuestionBlock(result) {
   let question = result.question;
   let difficulty = result.difficulty;
   let category = result.category;
-  let questionBlock = document.createElement("section");questionBlock.innerHTML = `<section class="question"><h3>${question}</h3><small>Category: ${category}<br>Difficulty: ${difficulty}</small>`
+  let answerSet;
+  if (result.type === "multiple")
+    answerSet = makeAnswerList(result.correct_answer, result.incorrect_answers)
+    else answerSet = ["True", "False"];
+  console.log(answerSet);
+  let questionBlock = document.createElement("section");
+  if (answerSet.length == 2)
+    questionBlock.innerHTML = `<section class="question">
+    <h3>${question}</h3>
+    <small>${category}</small>
+    <fieldset>
+    <label>${answerSet[0]}<input type="checkbox" value="${answerSet[0]}"</label>
+    <label>${answerSet[1]}<input type="checkbox" value="${answerSet[1]}"</label>
+    </fieldset>`
+  else 
+  questionBlock.innerHTML = `<section class="question">
+    <h3>${question}</h3>
+    <small>${category}</small>
+    <fieldset class="flex-column">
+    <label><input type="checkbox" value="${answerSet[0]}">${answerSet[0]}</label>
+    <label><input type="checkbox" value="${answerSet[1]}">${answerSet[1]}</label>
+    <label><input type="checkbox" value="${answerSet[2]}">${answerSet[2]}</label>
+    <label><input type="checkbox" value="${answerSet[3]}">${answerSet[3]}</label>
+    </fieldset>`
   document.querySelector(".quiz-display").appendChild(questionBlock);
+};
+
+
+
+function makeAnswerList(trueString, answerArr) {
+  answerArr.push(trueString);
+  return shuffleArray(answerArr);
+}
+
+function shuffleArray(array) {
+  for (let i = array.length -1; i > 0; i--) {
+    let j = Math.floor(Math.random()*(i+1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 
-// structure is data.results[NUM]....
-
 /*
-  if (result.type = "multiple") {
-    let answerSet = result.incorrect_answers.push(result.correct_answer);
-    let answerSetFinal = shuffleArray(answerSet)
+ let answerSet;
+  let answerSetFinal;
+  if (result.type === "multiple") {
+    answerSet = result.incorrect_answers.slice().push(result.correct_answer);
+    answerSetFinal = shuffleArray(answerSet)
     }
-    else let answerSetFinal = ["True", "False"];
-  makeAnswerList(answerSetFinal);
+    else answerSetFinal = ["True", "False"];
+  console.log(answerSetFinal);
+  // makeAnswerList(answerSetFinal);
 
-  */
+
+
+    let answer;
+  let answerBlock = document.createElement("fieldset").appendChild(
+arr.forEach(e => {
+  answer = document.createElement("label");
+  answer.innerHTML = `${e}<input type="radio" value="${e}">`}))
+  return answerBlock;
+}
+
+*/
+
   
 // for multiple choice questions, need radio buttons for each one
 // for true/false questions, need two radio buttons
@@ -47,9 +96,3 @@ function makeQuestionBlock(result) {
 
 //run the answerSet through the random scrambler function
 
-function shuffleArray(arr) {
-  for (let i = array.length -1; i > 0; i--) {
-    let j = Math.floor(Math.random()*(i+1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
