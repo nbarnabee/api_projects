c4e785c6b19c30f2d5436f4599fac618a142b587
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-20 16:45:58 +0200

    feat: add timestamp check function

--- quote_of_the_day/main.js
+++ quote_of_the_day/main.js
@@ -1,13 +1,31 @@
+window.onload = checkQuote
 
+function checkQuote() {
+  if (localStorage.getItem("dailyQuote")) {
+    const current = new Date(), 
+      currentTime = current.getTime(),
+      currentDate = current.getDate(),
+      timeStored = localStorage.getItem("timeStored"),
+      dateStored = localStorage.getItem("dateStored");
+    if (dateStored != currentDate || currentTime > (timeStored + 86400000)) {
+      localStorage.clear();
+      getQuote()
+    }
+    else displayQuote(localStorage.getItem("dailyQuote"));
+  };
+  getQuote();
+}
 
-let select = document.querySelector("select");
-let category = select.options[select.selectedIndex].value;
-
-  fetch("")
+function getQuote() {
+  fetch("https://quotes.rest/qod?category=inspire&language=en")
   .then(response => response.json())
   .then(data => {
-    console.log(data);
+    displayQuote(data);
     localStorage.setItem("dailyQuote", data);
+    const now = new Date(),
+      time = now.getTime(),
+      date = now.getDay();
+    localStorage.setItem("timeStored", time);
+    localStorage.setItem("dateStored", date);
   })
-
-
+}
