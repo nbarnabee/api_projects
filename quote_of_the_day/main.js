e17b4f854ea8b449ea481f60436f3d858c9f3445
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-20 18:53:04 +0200

    Fix: resolve various issues

--- quote_of_the_day/main.js
+++ quote_of_the_day/main.js
@@ -11,19 +11,23 @@ function checkQuote() {
       console.log(dateStored != currentDate);
       console.log(currentTime > (timeStored + 86400000));
       localStorage.clear();
-      getQuote()
-      getPicture()
+      getQuote();
+      getPicture();
+      getTimeStamp();
     }
     else {
-      displayQuote(JSON.parse(localStorage.getItem("dailyQuote")), JSON.parse(localStorage.getItem("dailyPicture")));
+      let quote = JSON.parse(localStorage.getItem("dailyQuote"));
+      let picture = JSON.parse(localStorage.getItem("dailyPicture"));
+      displayQuote(quote);
+      displayPicture(picture);
     }
   }
   else {
     getQuote();
     getPicture();
+    getTimeStamp();
   }
 }
-
 
 function getQuote() {
   console.log("Making a new fetch request");
@@ -32,39 +36,34 @@ function getQuote() {
   .then(data => {
     displayQuote(data.contents.quotes[0]);
     localStorage.setItem("dailyQuote", JSON.stringify(data.contents.quotes[0]));
-    const now = new Date(),
-      time = now.getTime(),
-      date = now.getDay();
-    localStorage.setItem("timeStored", time);
-    localStorage.setItem("dateStored", date);
   })
 };
-
-
-function displayQuote(quote, picture) {
-  document.querySelector("blockquote").innerText = `${quote.quote}`;
-  document.querySelector("figcaption").innerHTML = `&#8212;${quote.author}`;
-  document.querySelector("small").innerHTML = `Quote courtesy of <a href="http://theysaidso.com">theysaidso.com</a>. Background image by <a
-      href="${picture.url}">${picture.author}</a>`
-};
-
-
-//data.quote
-//data.tags
-//
-
-// check picture ids and dimensions???
-// https://picsum.photos/WIDTHVAR/HEIGHTVAR
-//landscape ratio 16:9
-//1.78
-//width = height*1.78, so height = width/1.78
 
 function getPicture() {
   let id = Math.floor(Math.random() * 1000 + 1);
   fetch(`https://picsum.photos/id/${id}/info`)
   .then(response => response.json())
   .then(data => {
-    console.log(data)
+    displayPicture(data);
     localStorage.setItem("dailyPicture", JSON.stringify(data));
   });
 };
+
+
+function getTimeStamp() {
+  const now = new Date(),
+  time = now.getTime(),
+  date = now.getDay();
+  localStorage.setItem("timeStored", time);
+  localStorage.setItem("dateStored", date);
+};
+
+function displayQuote(quote) {
+  document.querySelector("blockquote").innerText = `${quote.quote}`;
+  document.querySelector("figcaption").innerHTML = `&#8212;${quote.author}`;
+};
+
+function displayPicture(picture) {
+  document.querySelector("small").innerHTML = `Quote courtesy of <a href="http://theysaidso.com">theysaidso.com</a>. Background image by <a
+      href="${picture.url}">${picture.author}</a>`
+}
