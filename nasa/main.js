4384650a8f3a9a63014b4e5bcf6bc12a439e8621
Nicole Barnabee <nicole.barnabee@gmail.com>
2022-04-23 15:41:53 +0200

    Feat: add min/max values to date picker

--- nasa/main.js
+++ nasa/main.js
@@ -1,9 +1,18 @@
-document.querySelector("button").onclick = getByDate;
+window.onload = setCurrentDate;
+
+function setCurrentDate() {
+  let now = new Date();
+  let today = new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate()));
+  document.getElementById('selected-date').valueAsDate = today;
+  document.getElementById("selected-date").max = now.toLocaleDateString("en-ca");
+}
+
+document.getElementById("date-picker").onclick = getByDate;
 let baseURL = "https://api.nasa.gov/planetary/apod?api_key=NGbXFaC948GisO5Nx2TmrXLKYXBa5dVQ2c5OjFKw";
 getPOTD(baseURL);
 
 function getByDate() {
-  let dateValue = document.querySelector("input").value;
+  let dateValue = document.getElementById("selected-date").value;
   let date = `https://api.nasa.gov/planetary/apod?api_key=NGbXFaC948GisO5Nx2TmrXLKYXBa5dVQ2c5OjFKw&date=${dateValue}`;
 
   getPOTD(date);
